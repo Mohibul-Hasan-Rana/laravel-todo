739e332f98e95566b2d6a76431810e3c98bdefa8
mohibul <rana.csesust@gmail.com>
2025-07-10 18:26:12 +0600

    updated api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,13 +10,18 @@ class ApiController extends Controller
     public function fetchPosts()
     {
         try {
-            $response = Http::get('https://jsonplaceholder.typicode.com/posts', [
-                '_limit' => 10
-            ]);
-
+            $response = Http::get('https://jsonplaceholder.typicode.com/posts');
             if ($response->successful()) {
-                return $response->json();
+                $posts = collect($response->json())->take(10);
+                return $posts;
             }
+            // $response = Http::get('https://jsonplaceholder.typicode.com/posts', [
+            //     '_limit' => 5
+            // ]);
+
+            // if ($response->successful()) {
+            //     return $response->json();
+            // }
             
             return [];
         } catch (\Exception $e) {
